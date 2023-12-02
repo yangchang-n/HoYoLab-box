967e8ad077c2c1ac6d1fab56b66b0fb6a806570d
Yang Changeun <tomtomato@naver.com>
2023-12-02 21:46:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -29,14 +29,14 @@ def get_data_from_hoyolab(hoyo_uid, hoyo_token) :
                 return_list.append(['🎮 Genshin Impact'])
             elif eachGame['game_id'] == 6 :
                 return_list.append(['🎮 Honkai: Star Rail'])
-            return_list[-1].append('⚔️ Lv.' + str(eachGame['level']) + '   ')
+            return_list[-1].append('⚔️ Lv.' + str(eachGame['level']) + '     ')
             for eachData in eachGame['data'] :
                 if 'Active' in eachData['name'] :
                     return_list[-1].append('🕹️ ' + eachData['value'].rjust(4, padding) + ' days ')
                 elif 'Characters' in eachData['name'] :
                     return_list[-1].append('🤝 ' + eachData['value'].rjust(2, padding) + ' chars  ')
                 elif 'Achievements' in eachData['name'] :
-                    return_list[-1].append('🏆 ' + eachData['value'].rjust(4, padding) + ' achievements')
+                    return_list[-1].append('🏆 ' + eachData['value'].rjust(4, padding) + ' achvmnts')
         return return_list
     else : return 'Error occured'
 
