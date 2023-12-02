579d32af42a7d8566f30b7c175f8d73c0095eaff
Yang Changeun <tomtomato@naver.com>
2023-12-02 13:17:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -20,39 +20,35 @@ def get_data_from_hoyolab(hoyo_uid, hoyo_token) :
         headers=headers
     )
 
+    padding = ' '
     return_list = []
     if requestData.status_code == 200 :
         jsonData = requestData.json()
         for eachGame in jsonData['data']['list'] :
             if eachGame['game_id'] == 2 :
-                return_list.append(['Genshin Impact'])
+                return_list.append(['🎮 Genshin Impact'])
             elif eachGame['game_id'] == 6 :
-                return_list.append(['Honkai: StarRail'])
-            return_list[-1].append(str(eachGame['level']))
+                return_list.append(['🎮 Honkai: StarRail'])
+            return_list[-1].append('⚔️ Lv.' + str(eachGame['level']) + '  ')
             for eachData in eachGame['data'] :
                 if 'Active' in eachData['name'] :
-                    return_list[-1].append(eachData['value'])
+                    return_list[-1].append('🚪 ' + eachData['value'].rjust(4, padding) + ' days ')
+                elif 'Characters' in eachData['name'] :
+                    return_list[-1].append('👨‍👧‍👦 ' + eachData['value'].rjust(3, padding) + ' chars ')
                 elif 'Achievements' in eachData['name'] :
-                    return_list[-1].append(eachData['value'])
+                    return_list[-1].append('🏆 ' + eachData['value'].rjust(4, padding) + ' achievements')
         return return_list
     else : return 'Error occured'
 
 def update_gist(gh_api_url, gh_token, gist_id, hoyo_data) :
 
-    padding = ' '
     str_hoyo_data = ''
-
     for game in hoyo_data :
-        str_hoyo_data += '🎮 ' + game[0] + '\n'
-        temp = '⚔️ Lv.' + game[1].rjust(2, padding) + \
-            '         🚪' + game[2].rjust(5, padding) + \
-            ' days     🏆' + game[3].rjust(5, padding) + \
-            ' achievements\n\n'
-        str_hoyo_data += temp
+        str_hoyo_data += game[0] + '\n' + game[1] + game[2] + game[3] + game[4] + '\n\n'
 
     data = {
-        'description' : '🎮 HoYoverse game stats',
-        'files' : {'Powered by HoYoLab-box' : {'content' : str_hoyo_data}}
+        'description' : '🎮 HoYoverse gameplay stats',
+        'files' : {'🎮 HoYoverse gameplay stats' : {'content' : str_hoyo_data}}
     }
 
     request = requests.patch(
